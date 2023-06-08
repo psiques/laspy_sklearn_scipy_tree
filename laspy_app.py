57080fb82d0ff67a5dab275c8b4b5a20bd2380fd
Thais Sousa <64991864+psiques@users.noreply.github.com>
2023-06-07 23:33:43 -0300

    Update laspy_app.py

--- laspy_app.py
+++ laspy_app.py
@@ -5,10 +5,81 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Código de rotulação de classes
+    # Código de rotulação de classes
 def rotular_classe(valor):
-    # ... código de rotulação de classes ...
-
+    if valor == 0:
+        return "Zero length line"
+    elif valor == 1:
+        return "Chão"
+    elif valor == 2:
+        return "Vegetação Baixa"
+    elif valor == 3:
+        return "Vegetação Média"
+    elif valor == 4:
+        return "Vegetação Alta"
+    elif valor == 5:
+        return "Prédios"
+    elif valor == 6:
+        return "Terraço ou Telhado"
+    elif valor == 7:
+        return "Rio/Lago navegável"
+    elif valor == 8:
+        return "Rio/Lago não navegável"
+    elif valor == 9:
+        return "Ferrovias"
+    elif valor == 10:
+        return "Rua - Tipo 1"
+    elif valor == 11:
+        return "Rua - Tipo 2"
+    elif valor == 12:
+        return "Rua - Tipo 3"
+    elif valor == 13:
+        return "Rua - Tipo 4"
+    elif valor == 14:
+        return "Ponte"
+    elif valor == 15:
+        return "Condutores nus"
+    elif valor == 16:
+        return "Cabos aéreos Elicord"
+    elif valor == 17:
+        return "Postes"
+    elif valor == 18:
+        return "Linhas aéreas HV"
+    elif valor == 19:
+        return "Linhas aéreas MV"
+    elif valor == 20:
+        return "Linhas aéreas LV"
+    elif valor == 21:
+        return "Linhas aéreas de outros tipos"
+    elif valor == 22:
+        return "Cabos diversos"
+    elif valor == 23:
+        return "Interferência de Vegetação_1"
+    elif valor == 24:
+        return "Interferência de Vegetação_2"
+    elif valor == 25:
+        return "Interferência de Vegetação_3"
+    elif valor == 26:
+        return "Interferência entre cabos_1"
+    elif valor == 27:
+        return "Interferência entre cabos_2"
+    elif valor == 28:
+        return "Interferência entre cabos_3"
+    elif valor == 29:
+        return "Interferência de Edificação_1"
+    elif valor == 30:
+        return "Interferência de Edificação_2"
+    elif valor == 31:
+        return "Interferência de Edificação_3"
+    elif valor == 32:
+        return "Model Keypoints"
+    elif valor == 33:
+        return "Default"
+    elif valor == 34:
+        return "Low point"
+    else:
+        return "Classe não identificada"
+  
 file_path = input("Digite o caminho do arquivo LAS/LAZ: ")
 infile = laspy.read(file_path)
 
